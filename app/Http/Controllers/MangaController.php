718c77efff19e1c333b12504b670b017b47a4e16
Neochees <113420296+Neochees@users.noreply.github.com>
2024-09-11 16:32:50 +0200

    Crud begin, data allemaal goed gefetched

--- app/Http/Controllers/MangaController.php
+++ app/Http/Controllers/MangaController.php
@@ -2,9 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Manga;
 use Illuminate\Http\Request;
 
 class MangaController extends Controller
 {
-    //
+    public function index()
+    {
+        $mangas = Manga::get()->all();
+        return view('mangas.index', compact('mangas'));
+    }
+
+    public function show()
+    {
+        $mangas = Manga::all();
+        return view('show', ['mangas' => $mangas]);
+    }
+
+    public function create()
+    {
+        return view('mangas.create');
+    }
+    public function store()
+    {
+        $attributes = request()->validate([
+            'title' => 'required|max:225|unique:mangas,title',
+            'author' => 'required|max:225|unique:mangas,author',
+            'rating' => 'required|max:225',
+            'description' => 'required|max:225'
+        ]);
+    }
+
+    public function edit(Manga $manga)
+    {
+        return view('mangas.edit', compact('manga'));
+    }
+
+    public function update(Manga $manga)
+    {
+
+    }
+
+    public function destroy(Manga $manga)
+    {
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MangaController;
 use App\Models\Manga;
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', [MangaController::class, 'index']);
+
+Route::get('/manga/{manga}', [MangaController::class, 'show']);
+
+Route::post('/manga', [MangaController::class, 'store']);
+
+Route::patch('/manga/{manga}', [MangaController::class, 'update']);
