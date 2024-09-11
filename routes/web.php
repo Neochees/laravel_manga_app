ef1cf7d95888412908252068d9231118861307ff
Neochees <113420296+Neochees@users.noreply.github.com>
2024-09-11 10:09:32 +0200

    stap 8 af, routes, controller end database done

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MangaController;
+use App\Models\Manga;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
