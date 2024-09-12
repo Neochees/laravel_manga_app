269dc1ba0d4771b94422300f4ed5211d695081d5
Neochees <113420296+Neochees@users.noreply.github.com>
2024-09-12 17:00:43 +0200

    Genre laat goed zien

---
+++ resources/views/genres/index.blade.php
@@ -0,0 +1,27 @@
+<x-layout>
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+
+        @foreach($genres as $genre)
+            <section class="p-6 bg-gray-100 rounded-lg shadow-lg">
+                <h6 class="text-sm font-medium text-gray-500">{{ $genre }}</h6>
+                <h6 class="text-sm font-medium text-yellow-500">Amount: </h6>
+                          {{--How many manga are in this genre--}}
+                <a href="/manga/{{ $genre->id }}" class="rounded-xl border border-blue-500 text-white bg-blue-500 py-2 px-4 hover:bg-white hover:text-blue-500"><button class="mt-10">
+                        Show
+                    </button></a>
+                <a href="/manga/{{ $genre->id }}/edit" class="rounded-xl border border-green-500 text-white bg-green-500 py-2 px-4 hover:bg-white hover:text-green-500"><button class="mt-10">
+                        Edit
+                    </button></a>
+                <form method="POST" action="/manga/{{ $genre->id }}" class="inline-block">
+                    @csrf
+                    @method('DELETE')
+                    <a href="/"><button type="submit" class="rounded-xl text-white bg-red-500 py-2 px-4">
+                            DELETE
+                        </button></a>
+                </form>
+
+            </section>
+        @endforeach
+        <a href="/create"><button class="rounded-xl text-white bg-blue-500 py-2 px-4">Create+</button></a>
+    </main>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MangaController;
+use App\Http\Controllers\GenreController;
 use App\Models\Manga;
 
 Route::get('/', [MangaController::class, 'index']);
-
 Route::get('/manga/{manga}', [MangaController::class, 'show']);
-
 Route::get('/manga/{manga}/edit', [MangaController::class, 'edit']);
-
 Route::patch('/manga/{manga}', [MangaController::class, 'update']);
-
 Route::post('/create', [MangaController::class, 'store']);
-
 Route::get('/create', [MangaController::class, 'create']);
-
 Route::delete('/manga/{manga}', [MangaController::class, 'destroy']);
+
+Route::get('/genre', [GenreController::class, 'index']);
